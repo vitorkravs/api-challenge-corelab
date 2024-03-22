9c605b66a055cfecb2bd01d544891ae49aca355a
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-21 23:06:06 -0300

    feat: rotas prontas

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,16 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const NotesController = () => import('#controllers/notes_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.get('/api/notes', [NotesController, 'index'])
+router.post('/api/notes/post', [NotesController, 'create'])
+router.put('/api/notes/update/:id', [NotesController, 'update'])
+router.delete('/api/notes/delete/:id', [NotesController, 'delete'])
+router.patch('/api/notes/toggleIsFavorite/:id', [NotesController, 'toogleIsFavorite'])
