b27f20d1f0bec97a7acfdf63f3d08d3aa1b4c609
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-21 23:05:53 -0300

    feat: model do db

---
+++ app/models/note.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Note extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare title: string
+
+  @column()
+  declare annotation: string
+
+  @column()
+  declare is_favorite: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
